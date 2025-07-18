afd5522485c7fe350738b85d7209eb3640dd4815
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-18 19:17:16 +0530

    applied deletion functionality , line through functionality , checked and unchecked functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,44 @@ import './App.css'
 function App() {
   const [input, setInput] = useState('')
   const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      taskName: "Eating food",
-      completed: false
-    },
-    {
-      id: 2,
-      taskName: "Eating food 2",
-      completed: true
-    }
+
   ])
 
-  function handleAddition()
-  {
-    let data ={
-      id : tasks.length,
-      taskName : input,
-      completed : false
+  function handleAddition() {
+    if (input.trim() === "") {
+      setInput("")
+      return
     }
-    setTasks((prev)=> {
-      return [...prev , data]
-    } )
+    let data = {
+      id: tasks.length,
+      taskName: input,
+      completed: false
+    }
+    setTasks((prev) => {
+      return [...prev, data]
+    })
+    setInput("")
+  }
+  function handleChange(taskid) {
+    setTasks((prev) => (
+      prev.map((task) => {
+        if (task.id === taskid) {
+          return {
+            ...task, completed: !task.completed
+          }
+        }
+        else {
+          return { ...task }
+        }
+      })
+    )
+
+    )
+
+  }
+
+  function handleDelete(taskid) {
+    setTasks(tasks.filter((task) => task.id !== taskid))
   }
 
   return (
@@ -37,9 +53,12 @@ function App() {
 
       </div>
       <div>
-        <ol>
-          {tasks.map((task, i) => <li key={i}> <input type='checkbox' checked={task.completed} />{task.taskName} <button>Delete</button></li>)}
-        </ol>
+        {
+          tasks.length === 0 ? <h3>No Tasks</h3> : <ol>
+            {tasks.map((task) => <li key={task.id}> <input type='checkbox' checked={task.completed} onChange={() => handleChange(task.id)} /><span className={task.completed ? "strike": ""}>{task.taskName}</span> <button onClick={() => handleDelete(task.id)}>Delete</button></li>)}
+          </ol>
+        }
+
 
       </div>
     </div>
